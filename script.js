1f6f32e874fa9ba751d92b58b032daf3fbc72e3d
jvondzerza <jvondzerza@googlemail.com>
2021-11-18 09:58:27 +0100

    finished core design

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let timelineDay = gsap.timeline();
 let timelineNight = gsap.timeline();
 
 timelineDay
-    .fromTo('.clouds-daytime', 3, {opacity: 0}, {opacity: 1, y: 300})
-    .to('.city', 3, {y: 80}, '-=3')
-    .fromTo('.bg1', 3, {y: -50}, {y: 0, duration: 3}, '-=3')
-    .fromTo('.day-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5}, '-=3')
-    .fromTo('.text', 3, {opacity: 0}, {opacity: 1}, '-=3')
 
+    .fromTo('.clouds-daytime', 3, {opacity: 0}, {opacity: 1, y: 300, duration: 3})
+    .fromTo('.asset-credits', 0.2, {opacity: 1}, {opacity: 0, duration: 3}, '-=3')
+    .to('.city', 3, {y: 80, duration: 3}, '-=3')
+    .fromTo('.bg1', 3, {y: 0}, {y: -50, duration: 3}, '-=3')
+    .fromTo('.day-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5, duration: 3}, '-=3')
+    .fromTo('.text', 3, {opacity: 0, x: -200}, {opacity: 1, x:0, duration: 3}, '-=3')
 
 let sceneDay = new ScrollMagic.Scene({
     triggerElement: "section",
@@ -20,14 +21,10 @@ let sceneDay = new ScrollMagic.Scene({
     .addTo(controller);
 
 timelineNight
-    .fromTo('.clouds-night', 3, {opacity: 1, y: -50}, {opacity: 0.9, y: 100})
-    .to('.mountains', 3, {y: 80}, '-=3')
-    .fromTo('.bg2', 3, {y: 0}, {y: -50, duration: 3}, '-=1')
-    .fromTo('.night-title', 3, {opacity: 0}, {opacity: 1}, '-=3')
-
-
-
-
+    .fromTo('.clouds-night', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60, duration: 3})
+    .fromTo('.mountains', 3, {y: -50}, {y: 25, duration: 3}, '-=3')
+    .fromTo('.bg2', 2, {y: 0}, {y: -300, duration: 3}, '-=3')
+    .fromTo('.night-title', 3, {opacity: 0}, {opacity: 1, duration: 3}, '-=3')
 
 let sceneNight = new ScrollMagic.Scene({
     triggerElement: ".night",
