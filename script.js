2474d6d6cb2fbaa806932398efffab939ca33b53
jvondzerza <jvondzerza@googlemail.com>
2021-11-18 23:38:58 +0100

    minor edits, going to add horizontal paralax to both background sections tomorrow

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let controller = new ScrollMagic.Controller();
 let timelineDay = gsap.timeline();
 let timelineEvening = gsap.timeline();
-let timelineCloud = gsap.timeline({paused: false})
+let timelineCloud = gsap.timeline({paused: false});
 
 timelineDay
 
     .fromTo('.clouds-daytime', 3, {opacity: 0}, {opacity: 1, y: 300, duration: 3})
-    .fromTo('.asset-credits', 0.2, {opacity: 1}, {opacity: 0, duration: 3}, '-=3')
-    .to('.city', 3, {y: 80, duration: 3}, '-=3')
-    .to('.sun', 3, {y: 400, x: -80, duration: 3, opacity: 0.1}, '-=3')
-    .fromTo('.bg1', 3, {y: 0}, {y: -50, duration: 3}, '-=3')
-    .fromTo('.day-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5, duration: 3}, '-=3')
-    .fromTo('.text', 3, {opacity: 0, y: 20}, {opacity: 1, y:0, duration: 3}, '-=3')
+    .fromTo('.asset-credits', 0.2, {opacity: 1}, {opacity: 0}, '-=3')
+    .to('.city', 3, {y: 120}, '-=3')
+    .to('.sun', 3, {y: 400, x: -80, opacity: 0.1}, '-=3')
+    .fromTo('.bg1', 3, {y: 0}, {y: -50}, '-=3')
+    .fromTo('.day-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5}, '-=3')
+    .fromTo('.text', 3, {opacity: 0, y: 20}, {opacity: 1, y:0}, '-=3')
 
 let sceneDay = new ScrollMagic.Scene({
     triggerElement: "section",
@@ -23,12 +23,12 @@ let sceneDay = new ScrollMagic.Scene({
     .addTo(controller);
 
 timelineEvening
-    .fromTo('.clouds-evening', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60, duration: 3})
-    .fromTo('.mountains', 3, {y: -50}, {y: 25, duration: 3}, '-=3')
-    .fromTo('.bg2', 2, {y: 0}, {y: -300, duration: 3}, '-=3')
-    .fromTo('.evening-title', 3, {opacity: 0, scale: 0.5}, {opacity: 1, scale: 1, duration: 3}, '-=3')
-    .to('.balloon', 3, {y: -750, x:300, duration: 3}, '-=3')
-    .to('.balloon2', 3, {y: -250, x:400, duration: 3}, '-=3')
+    .fromTo('.clouds-evening', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60})
+    .fromTo('.mountains', 3, {y: -50}, {y: 25}, '-=3')
+    .fromTo('.bg2', 2, {y: 0}, {y: -300}, '-=3')
+    .fromTo('.evening-title', 3, {opacity: 0, scale: 0.5}, {opacity: 1, scale: 1}, '-=3')
+    .to('.balloon', 3, {y: -750, x:300}, '-=3')
+    .to('.balloon2', 3, {y: -250, x:400}, '-=3')
 
 let sceneEvening = new ScrollMagic.Scene({
     triggerElement: ".evening",
@@ -45,5 +45,6 @@ let width = 1171,
     duration = width / speed,
     endPosition = width - (speed / 60);
 
-timelineCloud.to('.content', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone});
+timelineCloud.to('.content', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone})
 
+//timelineCloud.to('.day', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone})
