18ae9827351ccddad3ea7d2d380b54202cd8c931
jvondzerza <jvondzerza@googlemail.com>
2021-11-18 18:02:58 +0100

    added and animated a sun to the day section and some hot air ballons to the evening section, added asset credits, implemented an infinite horizontal scroll for the clouds in the content section

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let controller = new ScrollMagic.Controller();
 let timelineDay = gsap.timeline();
-let timelineNight = gsap.timeline();
+let timelineEvening = gsap.timeline();
+let timelineCloud = gsap.timeline({paused: false})
 
 timelineDay
 
     .fromTo('.clouds-daytime', 3, {opacity: 0}, {opacity: 1, y: 300, duration: 3})
     .fromTo('.asset-credits', 0.2, {opacity: 1}, {opacity: 0, duration: 3}, '-=3')
     .to('.city', 3, {y: 80, duration: 3}, '-=3')
+    .to('.sun', 3, {y: 400, x: -80, duration: 3, opacity: 0.1}, '-=3')
     .fromTo('.bg1', 3, {y: 0}, {y: -50, duration: 3}, '-=3')
     .fromTo('.day-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5, duration: 3}, '-=3')
-    .fromTo('.text', 3, {opacity: 0, x: -200}, {opacity: 1, x:0, duration: 3}, '-=3')
+    .fromTo('.text', 3, {opacity: 0, y: 20}, {opacity: 1, y:0, duration: 3}, '-=3')
 
 let sceneDay = new ScrollMagic.Scene({
     triggerElement: "section",
@@ -20,17 +22,28 @@ let sceneDay = new ScrollMagic.Scene({
     .setTween(timelineDay)
     .addTo(controller);
 
-timelineNight
-    .fromTo('.clouds-night', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60, duration: 3})
+timelineEvening
+    .fromTo('.clouds-evening', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60, duration: 3})
     .fromTo('.mountains', 3, {y: -50}, {y: 25, duration: 3}, '-=3')
     .fromTo('.bg2', 2, {y: 0}, {y: -300, duration: 3}, '-=3')
-    .fromTo('.night-title', 3, {opacity: 0}, {opacity: 1, duration: 3}, '-=3')
+    .fromTo('.evening-title', 3, {opacity: 0}, {opacity: 1, duration: 3}, '-=3')
+    .to('.balloon', 3, {y: -750, x:300, duration: 3}, '-=3')
+    .to('.balloon2', 3, {y: -250, x:400, duration: 3}, '-=3')
 
-let sceneNight = new ScrollMagic.Scene({
-    triggerElement: ".night",
+let sceneEvening = new ScrollMagic.Scene({
+    triggerElement: ".evening",
     duration: "100%",
     triggerHook: 0.5,
 })
 
-    .setTween(timelineNight)
+    .setTween(timelineEvening)
     .addTo(controller);
+
+
+let width = 1171,
+    speed = 60,
+    duration = width / speed,
+    endPosition = width - (speed / 60);
+
+timelineCloud.to('.content', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone});
+
