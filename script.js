a1106e35dc044f4f53120ae1415b8ad7ea42ec28
jvondzerza <jvondzerza@googlemail.com>
2021-11-19 12:30:34 +0100

    changed section themes, added and modified paralax features, working on a fix for animation smoothness

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
 let controller = new ScrollMagic.Controller();
-let timelineDay = gsap.timeline();
-let timelineEvening = gsap.timeline();
+let timelineNight = gsap.timeline();
+let timelineSunrise = gsap.timeline();
 let timelineCloud = gsap.timeline({paused: false});
+let timelineCloud2 = gsap.timeline({paused: false});
+let timelineCloud3 = gsap.timeline({paused: false});
+let timelineWing = gsap.timeline({paused: false});
 
-timelineDay
+let width = 1200,
+    speed = 5,
+    duration = width / speed,
+    endPosition = width - (speed / 60);
 
-    .fromTo('.clouds-daytime', 3, {opacity: 0}, {opacity: 1, y: 300, duration: 3})
+timelineCloud
+    .to('.night', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone});
+
+timelineNight
+    .fromTo('.clouds-night', 3, {opacity: 0}, {opacity: 1.1, y: 300, duration: 3})
     .fromTo('.asset-credits', 0.2, {opacity: 1}, {opacity: 0}, '-=3')
-    .to('.city', 3, {y: 120}, '-=3')
-    .to('.sun', 3, {y: 400, x: -80, opacity: 0.1}, '-=3')
+    .to('.city', 3, {y: 150}, '-=3')
+    .to('.moon', 3, {y: 400, x: -80, opacity: 0.1}, '-=3')
     .fromTo('.bg1', 3, {y: 0}, {y: -50}, '-=3')
-    .fromTo('.day-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5}, '-=3')
+    .fromTo('.night-title', 3,{color: '#92b5ce'}, {x: -100, color: '#E6EBF3', opacity: 0.5}, '-=3')
     .fromTo('.text', 3, {opacity: 0, y: 20}, {opacity: 1, y:0}, '-=3')
 
-let sceneDay = new ScrollMagic.Scene({
-    triggerElement: "section",
+let sceneNight = new ScrollMagic.Scene({
+    triggerElement: ".night",
     duration: "100%",
     triggerHook: 0,
 })
 
-    .setTween(timelineDay)
+    .setTween(timelineNight)
     .addTo(controller);
 
-timelineEvening
-    .fromTo('.clouds-evening', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60})
+timelineWing
+    .to('.wing', 60, {x: -2100, ease:Linear.easeNone, repeat: -1});
+
+let width2 = 1171,
+    speed2 = 60,
+    duration2 = width2 / speed2,
+    endPosition2 = width2 - (speed2 / 60);
+
+timelineCloud2.to('.content', duration2, {css:{backgroundPosition:endPosition2 + "px 0"}, repeat:-1, ease:Linear.easeNone});
+
+timelineSunrise
+    .fromTo('.clouds-sunrise', 3, {opacity: 0, y: -150}, {opacity: 1, y: 60, duration: 3})
     .fromTo('.mountains', 3, {y: -50}, {y: 25}, '-=3')
-    .fromTo('.bg2', 2, {y: 0}, {y: -300}, '-=3')
-    .fromTo('.evening-title', 3, {opacity: 0, scale: 0.5}, {opacity: 1, scale: 1}, '-=3')
+    .fromTo('.bg2', 3, {y: 0}, {y: -300}, '-=3')
+    .fromTo('.sunrise-title', 3, {opacity: 0, scale: 0.5}, {opacity: 1, scale: 1}, '-=3')
+    .fromTo('.sun', 3, {y: 100}, {y:20}, '-=3')
     .to('.balloon', 3, {y: -750, x:300}, '-=3')
     .to('.balloon2', 3, {y: -250, x:400}, '-=3')
 
-let sceneEvening = new ScrollMagic.Scene({
-    triggerElement: ".evening",
+let sceneSunrise = new ScrollMagic.Scene({
+    triggerElement: ".sunrise",
     duration: "100%",
     triggerHook: 0.5,
 })
 
-    .setTween(timelineEvening)
+    .setTween(timelineSunrise)
     .addTo(controller);
 
-
-let width = 1171,
-    speed = 60,
-    duration = width / speed,
-    endPosition = width - (speed / 60);
-
-timelineCloud.to('.content', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone})
-
-//timelineCloud.to('.day', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone})
+timelineCloud3
+    .to('.sunrise', duration, {css:{backgroundPosition:endPosition + "px 0"}, repeat:-1, ease:Linear.easeNone});
